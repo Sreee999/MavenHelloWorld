72d8239ad33dca1699471d486ba1ec0987558bf8
Sreee999 <edify99sree@gmail.com>
2025-05-26 19:55:29 +0530

    first commit

---
+++ src/test/java/com/big/demo/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package com.big.demo;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+	public void testHelloWorld() {
+		
+		System.out.println("Testing Hello World!!! ");
+	}
+
+}
